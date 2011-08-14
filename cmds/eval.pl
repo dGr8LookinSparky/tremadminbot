3d8454898d3f00352982374d56f53614ca5dbf2c
msk <mkpdev@gmail.com>
2011-08-14 07:37:00 -0400

    Fix eval for tokenize

--- cmds/eval.pl
+++ cmds/eval.pl
@@ -4,9 +4,9 @@ sub
 {
   my( $user, $acmdargs, $timestamp, $db ) = @_;
 
-  print( "Cmd: $user->{name} /eval ${acmdargs}\n" );
+  print( "Cmd: $user->{name} /eval @$acmdargs\n" );
 
-  my @response = eval( $acmdargs );
+  my @response = eval( "@$acmdargs" );
   @response = $@ =~ /^(.*) at .*? line \d+/ if( $@ );
   @response = split( /\n/, "@response" );
   replyToPlayer( $user, $_ ) foreach( @response );
