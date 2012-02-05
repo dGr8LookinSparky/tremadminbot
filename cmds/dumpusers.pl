c01328f87e0128a441de0168b0673cb24eb94efa
msk <mkpdev@gmail.com>
2012-02-05 15:07:04 -0500

    Add debug commands dumpusers and sql

---
+++ cmds/dumpusers.pl
@@ -0,0 +1,13 @@
+use common::sense;
+
+sub
+{
+  my( $user, $acmdargs, $timestamp, $db ) = @_;
+
+  my $q = $db->prepare( 'SELECT * FROM users' );
+  $q->execute;
+  while( my $r = $q->fetchrow_hashref )
+  {
+    replyToPlayer( $_[ 0 ], "  userID=$r->{ userID }; name=$r->{ name }; useCount=$r->{ useCount }; seenTime=$r->{ seenTime }; IP=$r->{ IP }; adminLevel=$r->{ adminLevel }; city=$r->{ city }; region=$r->{ region }; country=$r->{ country }" );
+  }
+};
---
+++ cmds/sql.pl
@@ -0,0 +1,18 @@
+use common::sense;
+
+sub
+{
+  my( $user, $acmdargs, $timestamp, $db ) = @_;
+
+  my $q = eval { $db->prepare( $acmdargs ) };
+  if( $@ && $@ =~ /^(.*) at .*? line \d+/ )
+  {
+    replyToPlayer( $user, "^3sql:^7 $1" );
+  }
+  return unless( $q );
+  $q->execute;
+  while( my @r = $q->fetchrow_array )
+  {
+    replyToPlayer( $user, "  '" . join( "', '", @r ) . "'" );
+  }
+};
