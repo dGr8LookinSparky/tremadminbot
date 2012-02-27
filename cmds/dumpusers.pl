eee4905542f2ee6393af9d6f764856308d5e511c
msk <mkpdev@gmail.com>
2012-02-27 06:46:56 -0500

    Remove adminLevel from users and seenTime from names

--- cmds/dumpusers.pl
+++ cmds/dumpusers.pl
@@ -8,6 +8,6 @@ sub
   $q->execute;
   while( my $r = $q->fetchrow_hashref )
   {
-    replyToPlayer( $_[ 0 ], "  userID=$r->{ userID }; name=$r->{ name }; useCount=$r->{ useCount }; seenTime=$r->{ seenTime }; IP=$r->{ IP }; adminLevel=$r->{ adminLevel }; city=$r->{ city }; region=$r->{ region }; country=$r->{ country }" );
+    replyToPlayer( $_[ 0 ], "  userID=$r->{ userID }; name=$r->{ name }; useCount=$r->{ useCount }; seenTime=$r->{ seenTime }; IP=$r->{ IP }; city=$r->{ city }; region=$r->{ region }; country=$r->{ country }" );
   }
 };
